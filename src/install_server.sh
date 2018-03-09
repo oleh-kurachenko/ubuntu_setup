45796d2ffd7213b811455421337e5148ddda6789
Oleh Kurachenko <oleh.kurachenko@gmail.com>
2018-03-09 23:49:50 +0200

    tmux installation added to server setup

--- src/install_server.sh
+++ src/install_server.sh
@@ -5,5 +5,7 @@ source constants.sh
 # welcome message
 echo -e "${BOLD_CYAN}#    installing server programs & tools${RESET_COLOR}"
 
+apt_action "install tmux"
+
 # exit message
 echo -e "${BOLD_CYAN}#    server programs & tools: ${BOLD_GREEN}INSTALLED!${RESET_COLOR}"
